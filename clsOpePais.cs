e961567d5949ad18a3e9554ca48d30e9d9646548
SuriiX <95985738+SuriiX@users.noreply.github.com>
2024-11-17 10:13:56 -0500

    Add files via upload

---
+++ clsOpePais.cs
@@ -0,0 +1,23 @@
+﻿using apiCineClub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiCineClub.Clases
+{
+    public class clsOpePais
+    {
+        private readonly bdCineClubEntities oCCE = new bdCineClubEntities();
+        public tblPai tblPais
+        {
+            get; set;
+        }
+        public List<tblPai> listarPais()
+        {
+            return oCCE.tblPais   //Tabla modelo entity
+                .OrderBy(x => x.Nombre)
+                .ToList();
+        }
+    }
+}
