e3c214ec67ecfae1a9ebe0f2bbfd428e63c34920
SuriiX <95985738+SuriiX@users.noreply.github.com>
2024-11-17 17:32:30 -0500

    Add files via upload

---
+++ peliculaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using apiCineClub.Models;
+using apiCineClub.Clases;
+namespace apiCineClub.Controllers
+{
+    [EnableCors(origins: "http://localhost:52006", headers: "*", methods: "*")]
+    public class peliculaController : ApiController
+    {
+        public IQueryable Get(int id, int comando )
+        {
+            clsOpePelicula opePeli = new clsOpePelicula();
+            IQueryable resultado;
+
+            if (comando == 1)
+            {
+                resultado = opePeli.listarPelicula(); 
+            }
+            else
+            {
+                resultado = opePeli.listarPeliculaXCod(id); 
+            }
+            return resultado;
+        }
+
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
